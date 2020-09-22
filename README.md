3fb8ec8fba7dfec63682035d7fb6b48bb7b5a334
javiercarpio57 <javierrr.carpio57@gmail.com>
2020-09-22 17:55:10 -0600

    FUNCTIONALITY: Send files. Readme updated

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# protocolo_xmpp
+# XMPP protocol
+
+## Installation
+Use the **requirements.txt** file to install python dependencies, as follow:
+```
+pip install -r requirements.txt
+```
+Then, run the program:
+```
+python xmpp_protocol.py
+```
+
+## Usage
+### Login and Register
+To register a new user, select option 1 and fill the username, password, name and email fields.
+Then, you can login with the created account.
+
+### Show all users/contacts
+After doing login, choose option 1.
+
+### Add user to contacts
+Choose option 2, and write the user's jid.
+
+### Show user's info
+Choose option 3, and write the user's jid that you want to get extra info.
+
+### Send private message to user/contact
+Choose option 4, and fill the receiver and message content fields.
+
+### Rooms
+Choose option 5, and to join or create a new room select 1, and write the room's jid. Otherwise, select 2 to send messages to groups that you've joined previously.
+
+## Presence stanza
+In other words, changing your status. Choose option 6 and fill the show and status fields to change your status.
+
+### Send files
+Choose option 8, it'll ask you to browse a file, and in this case, only images (jpg, jpeg, png).
+
+### Notifications
+You'll receive the following notifications:
+- When a contact added you
+- When a contact deleted you
+- When a contact got online
+- When a contact got offline
+
+### Other functionalities
+#### Remove a contact
+Choose option 7, and write the user's jid that you want to delete from your contact list.
+
+#### Unregister
+Choose option 9, and your account will be removed from the server.
--- xmpp_protocol.py
+++ xmpp_protocol.py
@@ -17,6 +17,7 @@ import base64
 from datetime import datetime
 import time
 
+# Print with colors
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -27,12 +28,15 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+# Get file's path
 def my_file():
-    filename = filedialog.askopenfilename( initialdir="C:/", title="select file", filetypes=(("all files", "*.*"), ("text files", "*.txt")))
+    image_formats= [("JPEG", "*.jpg"), ("JPG", "*.jpeg"), ("PNG", "*.png")]
+    filename = filedialog.askopenfilename( initialdir="C:/", title="select file", filetypes=image_formats)
 
     return filename
 
 class Usuario():
+    # Initialize User variables
     def __init__(self, user, status, show, subscription, online):
         self.user = user
         self.show = show
@@ -40,21 +44,26 @@ class Usuario():
         self.subscription = subscription
         self.online = online
 
+    # Set state of User
     def set_states(self, status, show):
         self.status = status
         self.show = show
 
+    # Change online in on/off
     def set_online(self, online):
         self.online = online
 
+    # Get User username
     def get_username(self):
         return self.user
 
+    # Get all info of User
     def get_usuario(self):
         return [self.user, self.status, self.show, self.online, self.subscription]
 
 class Cliente(ClientXMPP):
 
+    # Initialize Cliente variables
     def __init__(self, jid, password):
         ClientXMPP.__init__(self, jid, password)
         self.timeout = 45
@@ -63,6 +72,7 @@ class Cliente(ClientXMPP):
         self.rooms = {}
         self.contador_rooms = 1
 
+        # Manage all events
         self.add_event_handler('session_start', self.session_start)
         self.add_event_handler('message', self.message)
         self.add_event_handler('changed_status', self.on_presence)
@@ -71,6 +81,7 @@ class Cliente(ClientXMPP):
         self.add_event_handler("got_offline", self.got_offline)
         self.add_event_handler("got_online", self.got_online)
         
+        # Register all plug-ins
         self.register_plugin('xep_0030') # Service Discovery
         self.register_plugin('xep_0199') # XMPP Ping
         self.register_plugin('xep_0004') # Data forms
@@ -81,6 +92,7 @@ class Cliente(ClientXMPP):
         import ssl
         self.ssl_version = ssl.PROTOCOL_TLS
         
+    # Trigger when someone unsubscribe
     def presence_unsubscribe(self, presence):
         person = self.jid_to_user(presence['from'])
 
@@ -90,6 +102,7 @@ class Cliente(ClientXMPP):
                 self.usuarios.pop(i)
                 break
 
+    # Trigger when someone subscribe
     def presence_subscribe(self, presence):
         person = self.jid_to_user(presence['from'])
 
@@ -99,8 +112,9 @@ class Cliente(ClientXMPP):
         u = Usuario(person, '---', '---', 'both', 'No')
         self.usuarios.append(u)
 
+    # Trigger when someone got offline
     def got_offline(self, presence):        
-        if self.jid not in str(presence['from']):
+        if self.boundjid.bare not in str(presence['from']):
             u = self.jid_to_user(str(presence['from']))
             print(bcolors.FAIL + '-- %s está en offline -- ' %(u) + bcolors.ENDC)
 
@@ -109,6 +123,7 @@ class Cliente(ClientXMPP):
                     i.set_online('No')
                     break
 
+    # Trigger when someone got online
     def got_online(self, presence):
         isGroup = self.IsGroup(str(presence['from']))
         if isGroup:
@@ -116,7 +131,7 @@ class Cliente(ClientXMPP):
             groupname = str(presence['from']).split('@')[0]
             print(bcolors.OKGREEN + '-- %s está en %s -- ' %(nick, groupname) + bcolors.ENDC)
         else:
-            if self.jid not in str(presence['from']):
+            if self.boundjid.bare not in str(presence['from']):
                 u = self.jid_to_user(str(presence['from']))
                 print(bcolors.OKGREEN + '-- %s está en online -- ' %(u) + bcolors.ENDC)
 
@@ -125,6 +140,7 @@ class Cliente(ClientXMPP):
                         i.set_online('Sí')
                         break
 
+    # Trigger when User wants to connect to server
     def session_start(self, event):
         self.send_presence(pshow='chat', pstatus='Quiero dormirrr')
         roster = self.get_roster()
@@ -141,6 +157,7 @@ class Cliente(ClientXMPP):
                 u = Usuario(user, status, show, subs, 'No')
                 self.usuarios.append(u)
 
+    # Trigger when someone sends a private chat and groupchat
     def message(self, msg):
         if msg['type'] in ('chat', 'normal'):
             if msg['subject'] == 'send_file':
@@ -155,6 +172,7 @@ class Cliente(ClientXMPP):
         if str(msg['type']) == 'groupchat':
             print(bcolors.WARNING + '\t--> (%(from)s): %(body)s' %(msg) + bcolors.ENDC)
 
+    # Trigger when someone changed status
     def on_presence(self, presence):
         user = str(presence['from'])
         user = self.jid_to_user(user)
@@ -187,18 +205,22 @@ class Cliente(ClientXMPP):
                 u = Usuario(user, status, show)
                 self.usuarios.append(u)
 
+    # Check if jid has 'conference'
     def IsGroup(self, from_jid):
         if 'conference' in from_jid.split('@')[1]:
             return True
         else:
             return False
 
+    # Send message to jid
     def SendMessageTo(self, jid, message):
         self.send_message(mto=jid, mbody=message, mtype='chat')
 
+    # Send message to room
     def SendMessageRoom(self, room, message):
         self.send_message(mto=room, mbody=message, mtype='groupchat')
 
+    # Let user to login
     def Login(self):
         success = False
         if self.connect():
@@ -210,16 +232,18 @@ class Cliente(ClientXMPP):
 
         return success
 
+    # Send subscription presence to jid
     def AddUser(self, jid):
         self.send_presence_subscription(pto=jid)
 
+    # Send file to jid
     def SendFile(self, path, to):
         with open(path, 'rb') as img:
             file_ = base64.b64encode(img.read()).decode('utf-8')
 
         self.send_message(mto=to, mbody=file_, msubject='send_file', mtype='chat')
         
-
+    # Get a user info
     def GetUser(self, username):
         iq = self.Iq()
         iq['type'] = 'set'
@@ -265,6 +289,7 @@ class Cliente(ClientXMPP):
                 us.append(i.get_usuario())
         return us, data
 
+    # Get all users info
     def GetUsers(self):
 
         iq = self.Iq()
@@ -286,35 +311,41 @@ class Cliente(ClientXMPP):
                                 </x> \
                               </query>")
         iq.append(item)
-        res = iq.send()
+        try:
+            res = iq.send()
+            data = []
+            temp = []
+            cont = 0
+            for i in res.findall('.//{jabber:x:data}value'):
+                cont += 1
+                txt = ''
+                if i.text == None:
+                    txt = '--'
+                else:
+                    txt = i.text
+
+                temp.append(txt)
+                if cont == 4:
+                    cont = 0
+                    data.append(temp)
+                    temp = []
+
+            us = []
+            for i in self.usuarios:
+                us.append(i.get_usuario())
+
+            return us, data
+        except IqTimeout:
+            print('Sin respuesta del server')
+            return [], []
         
-        data = []
-        temp = []
-        cont = 0
-        for i in res.findall('.//{jabber:x:data}value'):
-            cont += 1
-            txt = ''
-            if i.text == None:
-                txt = '--'
-            else:
-                txt = i.text
 
-            temp.append(txt)
-            if cont == 4:
-                cont = 0
-                data.append(temp)
-                temp = []
-
-        us = []
-        for i in self.usuarios:
-            us.append(i.get_usuario())
-
-        return us, data
-
+    # Transform jid to only username
     def jid_to_user(self, jid):
         jid = str(jid)
         return jid.split('@')[0]
 
+    # Remove a user from contact list
     def RemoveUser(self, jid):
         self.del_roster_item(jid)
 
@@ -324,27 +355,33 @@ class Cliente(ClientXMPP):
                 self.usuarios.pop(i)
                 break
 
+    # Change status of user
     def ChangeStatus(self, show, status):
         self.send_presence(pshow=show, pstatus=status)
 
+    # Let user to join or create a room
     def JoinOrCreateRoom(self, room, nickname):
         self.plugin['xep_0045'].joinMUC(room, nickname, wait=True)
         self.rooms[str(self.contador_rooms)] = room
         self.contador_rooms += 1
 
+    # Remove user from server
     def Unregister(self):
         iq = self.make_iq_set(ito='redes2020.xyz', ifrom=self.boundjid.user)
         item = ET.fromstring("<query xmlns='jabber:iq:register'> \
                                 <remove/> \
                               </query>")
         iq.append(item)
-        res = iq.send()
-        if res['type'] == 'result':
-            print(bcolors.OKGREEN + 'Cuenta eliminada' + bcolors.ENDC)
 
-        
+        try:
+            res = iq.send()
+            if res['type'] == 'result':
+                print(bcolors.OKGREEN + 'Cuenta eliminada' + bcolors.ENDC)
+        except IqTimeout:
+            print('Sin respuesta del server')
 
 class RegisterUser(sleekxmpp.ClientXMPP):
+    # Initialize variables
     def __init__(self, jid, password, name, email):
         sleekxmpp.ClientXMPP.__init__(self, jid, password)
         self.name = name
@@ -358,12 +395,14 @@ class RegisterUser(sleekxmpp.ClientXMPP):
         self.register_plugin('xep_0066') # Out-of-band Data
         self.register_plugin('xep_0077') # In-band Registration
 
+    # Trigger when user wants to connect
     def start(self, event):
         self.send_presence()
         self.get_roster()
 
         self.disconnect()
 
+    # Used to register a new user in server
     def register(self, iq):
         resp = self.Iq()
         resp['type'] = 'set'
@@ -383,7 +422,6 @@ class RegisterUser(sleekxmpp.ClientXMPP):
             self.disconnect()
 
 if __name__ == '__main__':
-
     domain = '@redes2020.xyz'
     show_state = {
         '1': 'chat',
@@ -517,8 +555,9 @@ if __name__ == '__main__':
             
             elif opcion == '8':
                 path = my_file()
-                to_person = input('Ingresa el jid para enviar archivo: ')
-                xmpp.SendFile(path, to_person)
+                if path:
+                    to_person = input('Ingresa el jid para enviar el archivo: ')
+                    xmpp.SendFile(path, to_person)
 
             elif opcion == '9':
                 xmpp.Unregister()
